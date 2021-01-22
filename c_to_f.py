563baedbd62b63ac1ae324b487928b7d1d0af943
lycTJ <49508677+lycTJ@users.noreply.github.com>
2021-01-23 01:53:05 +0800

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 22:27:01 2021
+
+@author: yclin
+"""
+
+celsius = input('請輸入攝氏溫度 : ')
+
+fahrenheit = float(celsius) * 9 / 5 + 32
+print('華氏溫度為 : ', fahrenheit)
